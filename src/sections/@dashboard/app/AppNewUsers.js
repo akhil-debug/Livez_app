025d4c1b57b31f10b7f3ce8429c86b5336081dbf
alenscaria <alenantony05@gmail.com>
2022-03-08 14:06:20 +0530

    indian flag added

--- src/sections/@dashboard/app/AppNewUsers.js
+++ src/sections/@dashboard/app/AppNewUsers.js
@@ -39,9 +39,6 @@ const TOTAL = 100;
 export default function AppNewUsers() {
   return (
     <RootStyle>
-      <IconWrapperStyle>
-        <Iconify icon="ant-design:apple-filled" width={24} height={24} />
-      </IconWrapperStyle>
       <Typography variant="h3">{fShortenNumber(TOTAL)}</Typography>
       <Typography variant="subtitle2" sx={{ opacity: 0.72 }}>
         NEW REGISTRATIONS
--- src/sections/@dashboard/app/AppWeeklySales.js
+++ src/sections/@dashboard/app/AppWeeklySales.js
@@ -39,9 +39,6 @@ const TOTAL = 1000;
 export default function AppWeeklySales() {
   return (
     <RootStyle>
-      <IconWrapperStyle>
-        <Iconify icon="ant-design:android-filled" width={24} height={24} />
-      </IconWrapperStyle>
       <Typography variant="h3">{fShortenNumber(TOTAL)}</Typography>
       <Typography variant="subtitle2" sx={{ opacity: 0.72 }}>
         SALES
